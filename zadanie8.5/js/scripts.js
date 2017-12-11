704aadd3a396eb0abe4283634f23f51548bb2291
Stefan Tórz <storz@interia.eu>
2017-12-11 17:56:23 +0100

    add files

---
+++ zadanie8.5/js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+var a = 5;
+var h = 3;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea)
---
+++ zadanie8.5alt/js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+var a = prompt('Enter the base of the triangle')
+var h = prompt('Enter the height of the triangle');
+var triangleArea = a*h/2;
+alert('The triangle area is '+triangleArea)
